fcd0c6e0f459c997f73a75ce66b01254a375cc64
Josh Gillette <joshtgillette@gmail.com>
2025-11-18 10:45:26 -0600

    Ignore SoFi Vault "transfers" in transactions

--- src/banking/accounts/sofi.py
+++ src/banking/accounts/sofi.py
@@ -18,6 +18,10 @@ class SoFi(BankAccount):
             'description': self.raw_transactions['Description']
         }).sort_values('date').reset_index(drop=True)
 
+        # Ignore Vault transactions, as they are one-sided transfers despite being
+        # a zero-sum transfer with respect to the account. Should this be a category?
+        self.transactions = self.transactions[~self.transactions['description'].str.contains('Vault')]
+
     def is_transaction_income(self, transaction: pd.Series) -> bool:
         """Determine if a normalized transaction is income."""
         return (super().is_transaction_income(transaction) and
