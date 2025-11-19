57f92bd7d3d76f2b43d3338b41546c9c430d3a92
Josh Gillette <joshtgillette@gmail.com>
2025-11-18 20:50:12 -0600

    Squashed commit of the following:
    commit 3b75f9f688a6d16edbcf052ef2208777063dd3c3
    Author: Josh Gillette <joshtgillette@gmail.com>
    Date:   Tue Nov 18 20:49:47 2025 -0600
    
        Track rewards/gifts

---
+++ src/tracking/categories/rewards.py
@@ -0,0 +1,16 @@
+from tracking.category import Category
+from banking.accounts.bank_account import BankAccount
+import pandas as pd
+from datetime import datetime
+
+class Rewards(Category):
+
+    def __init__(self, label):
+        super().__init__(label)
+
+    def filter_function(self, account: BankAccount, transaction: pd.Series):
+        return transaction.amount > 0 and \
+                (transaction.description == "Money Promo Bonus" or \
+                 transaction.description == "Money Referral Bonus" or \
+                 "CHASE CREDIT CRD RWRD RDM ACH CREDIT" in transaction.description or \
+                 (transaction.date.date() == datetime(2025, 8, 25).date() and account.name == "SoFi Checking" and transaction.description == "VENMO"))
