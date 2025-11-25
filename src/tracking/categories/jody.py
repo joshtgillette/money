8a720bfb3afa8a78872ba2aef5c1f37cf418ce4c
Josh Gillette <joshtgillette@gmail.com>
2025-11-24 23:17:29 -0500

    Remove unused imports in jody category

--- src/tracking/categories/jody.py
+++ src/tracking/categories/jody.py
@@ -1,5 +1,4 @@
 from tracking.category import Category
-from banking.accounts.bank_account import BankAccount
 import pandas as pd
 
 class Jody(Category):
@@ -7,5 +6,5 @@ class Jody(Category):
     def __init__(self, label):
         super().__init__(label)
 
-    def filter_function(self, account: BankAccount, transaction: pd.Series):
+    def filter_function(self, _, transaction: pd.Series):
         return transaction.description == "ESL FEDERAL CU P2P ACH WEB PAYMENT JOSHUA GILLETTE"
