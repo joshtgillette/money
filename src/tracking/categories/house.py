101180f93e3e223567bd71114b0a9a2d44befbd6
Josh Gillette <joshtgillette@gmail.com>
2025-11-26 22:49:08 -0500

    Update house category filter function

--- src/tracking/categories/house.py
+++ src/tracking/categories/house.py
@@ -1,13 +1,15 @@
 from tracking.category import Category
+from accounts.adapters.account import Account
 import pandas as pd
 from datetime import datetime
+from accounts.adapters.credit.chase import Chase
 
 class House(Category):
 
     def __init__(self, label):
         super().__init__(label)
 
-    def filter_function(self, _, transaction: pd.Series):
+    def filter_function(self, account: Account, transaction: pd.Series):
         return "PAYMENT TO xxxxxx3890" in transaction.description or \
                 ("VENMO CASHOUT ACH CREDIT" in transaction.description and
                  abs(transaction.date - datetime(transaction.date.year, transaction.date.month, 1)).days <= 3 and
@@ -15,4 +17,6 @@ class House(Category):
                 ("VENMO CASHOUT ACH CREDIT" in transaction.description and
                  transaction.amount == 50 and
                  transaction.date >= datetime(2024, 9, 1)) or \
-                 "CHASE CREDIT CRD EPAY" in transaction.description and transaction.date >= datetime(2025, 9, 1)
+                 "CHASE CREDIT CRD EPAY" in transaction.description and transaction.date >= datetime(2025, 9, 1) or \
+                (isinstance(account, Chase) and
+                 transaction.date >= datetime(2025, 9, 1))
