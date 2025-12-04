fe9393c0cb8d90bdbec7b1bf71c4610c601b9e70
Josh Gillette <joshtgillette@gmail.com>
2025-12-04 16:58:30 -0600

    Report transactions monthly in addition to full

--- src/report.py
+++ src/report.py
@@ -5,14 +5,16 @@ import pandas as pd
 class Report:
 
     PATH = "report"
-    DATA_PATH = f"{PATH}/transactions"
+    FULL_DATA_PATH = f"{PATH}/transactions/full"
+    MONTHLY_DATA_PATH = f"{PATH}/transactions/monthly"
     NOTES_FILE = f"{PATH}/notes.txt"
 
     def __init__(self):
         # Clear report directory
         shutil.rmtree(self.PATH, ignore_errors=True)
         os.makedirs(self.PATH, exist_ok=True)
-        os.makedirs(self.DATA_PATH, exist_ok=True)
+        os.makedirs(self.FULL_DATA_PATH, exist_ok=True)
+        os.makedirs(self.MONTHLY_DATA_PATH, exist_ok=True)
 
     def note_header(self, label: str):
         with open(self.NOTES_FILE, "a") as note_file:
@@ -23,8 +25,21 @@ class Report:
             note_file.write(f"{message}\n")
 
     def write_transactions(self, transactions: pd.DataFrame, filename: str):
-        transactions.sort_values('date').to_csv(
-            f"{self.DATA_PATH}/{filename}.csv",
+        transactions = transactions.sort_values('date').reset_index(drop=True)
+
+        # Write full transactions
+        transactions.to_csv(
+            f"{self.FULL_DATA_PATH}/{filename}.csv",
             columns=['date', 'account', 'amount', 'description'],
             index=False
         )
+
+        # Write monthly transactions
+        for month, group in transactions.groupby(transactions['date'].dt.to_period('M')):
+            monthly_path = f"{self.MONTHLY_DATA_PATH}/{month.strftime('%m%y')}/{filename}.csv"
+            os.makedirs(os.path.dirname(monthly_path), exist_ok=True)
+            group.to_csv(
+                monthly_path,
+                columns=['date', 'account', 'amount', 'description'],
+                index=False
+            )
