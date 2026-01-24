ff5c5d0ee8b9ff2f20d963ce9fc332dd09cdc6cf
Josh Gillette <joshtgillette@gmail.com>
2026-01-24 14:54:40 -0600

    Implement interface and tagging
    - Leverage advisor to interface with user
    - Implement tagging with backend banker book

--- src/advisor.py
+++ src/advisor.py
@@ -1,6 +1,7 @@
 """Personal finance advisor that orchestrates transaction loading, tagging, and reporting."""
 
 from pathlib import Path
+from typing import Callable
 
 from adapters import ACCOUNT_ADAPTERS
 from banker import Banker
@@ -17,5 +18,50 @@ class Advisor:
 
     def advise(self) -> None:
         """Load transactions, apply tags, and generate organized transaction reports."""
+        print()
+
         # Direct the banker to load transactions for the provided accounts
         self.banker.load_account_transactions(self.SOURCE_TRANSACTIONS_PATH)
+
+        print()
+        OPTIONS: list[tuple[str, Callable]] = [("tag transactions", self.tag)]
+        while True:
+            # Display options
+            for num, (label, _) in enumerate(OPTIONS):
+                print(f"[{num + 1}] {label}", end="\t\t")
+            print()
+
+            # Get input and attempt to exectue option
+            user_input: str = input("option #: ")
+            if not user_input.strip():
+                break
+
+            print()
+            try:
+                OPTIONS[int(user_input) - 1][1]()
+            except ValueError:
+                print(f"error: input '{user_input}' is not a number")
+            except IndexError:
+                print(
+                    f"error: option # not from {min(1, len(OPTIONS))} to {len(OPTIONS)}"
+                )
+            print()
+        print()
+
+    def tag(self) -> None:
+        for transaction in self.banker:
+            # Skip already tagged transactions
+            if self.banker.get_existing_tags_for_transaction(transaction):
+                continue
+
+            # Display transaction
+            print(transaction)
+
+            # Tags are reqiured, if empty then exit tagging
+            tags = input("enter tag(s): ").strip()
+            if not tags:
+                return
+            tags = [tag.strip() for tag in tags.split(",")]
+
+            self.banker.write_book(transaction, tags)
+            print()
--- src/banker.py
+++ src/banker.py
@@ -1,7 +1,10 @@
 """Manages multiple bank accounts and provides transaction operations."""
 
+import json
 from pathlib import Path
-from typing import Dict
+from typing import Any, Dict, Iterator, List, cast
+
+import pandas as pd
 
 from account import Account
 
@@ -9,11 +12,14 @@ from account import Account
 class Banker:
     """Manages financial accounts and provides operations for transaction handling."""
 
+    BOOK_PATH: Path = Path("book.json")
+
     def __init__(self, *adapters: Account) -> None:
         """Initialize the banker with a collection of financial accounts."""
         self.accounts: Dict[str, Account] = {
             account.name.lower(): account for account in adapters
         }
+        self.transactions: pd.DataFrame
 
     def load_account_transactions(self, source_transactions_path: Path) -> None:
         """Load and normalize transactions from source CSV files for all accounts."""
@@ -35,7 +41,49 @@ class Banker:
         for account in self.accounts.values():
             account.normalize_source_transactions()
 
+        # Get aggregated transactions
+        self.transactions = pd.DataFrame(
+            [
+                transaction
+                for account in self.accounts.values()
+                for transaction in account.transactions.itertuples(index=True)
+            ]
+        ).sort_values("date", ascending=False)
+
         print(
             f"loaded {sum([len(account.transactions) for account in self.accounts.values()])}"
             f" transactions across {len(self.accounts)} accounts"
         )
+
+    def __iter__(self) -> Iterator[str]:
+        for transaction in self.transactions.itertuples(index=True):
+            transaction = cast(Any, transaction)
+            yield (
+                f"{transaction.account} on {pd.to_datetime(transaction.date).strftime('%B %d, %Y')} for "
+                f"{'-' if transaction.amount < 0 else ''}${abs(transaction.amount):,.2f} - {transaction.description}"
+            )
+
+    def write_book(self, transaction: str, tags: List[str]) -> None:
+        # Get up-to-date book
+        book = self.read_book()
+
+        # Update transaction tags from hash
+        book.update({transaction: book.get(transaction, []) + tags})
+
+        # Perform file write
+        with open(self.BOOK_PATH, "w") as book_file:
+            json.dump(book, book_file, indent=2)
+
+    def read_book(self) -> Dict:
+        book = {}
+        try:
+            # Perform file read
+            with open(self.BOOK_PATH, "r+") as book_file:
+                book = json.load(book_file)
+        except FileNotFoundError:
+            pass
+
+        return book
+
+    def get_existing_tags_for_transaction(self, transaction) -> List[str]:
+        return self.read_book().get(transaction, [])
